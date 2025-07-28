67221dac19ef60e6db9c374153e4d2c0591e4463
MitaliChandani <141425833+MitaliChandani@users.noreply.github.com>
2025-07-28 09:49:05 +0530

    String.py

--- DSA/Assignment/String.py
+++ DSA/Assignment/String.py
@@ -12,18 +12,14 @@ if s == s[::-1]:
 else:
     print("Not a palindrome")
 
-# 3. Given a string, return a new string made of n copies of the first 2 chars of the original string
-# where n is the length of the string. The string length will be >= 2.
-# Example: If input is "Wipro" then output should be "WiWiWiWiWi".
+# 3. Given a string, return a new string made of n copies of the first 2 chars of the original string where n is the length of the string. The string length will be >= 2. Example: If input is "Wipro" then output should be "WiWiWiWiWi".
 s = "Wipro"
 n = len(s)
 new_str = s[:2] * n
 print("Result:", new_str)
 
 
-#4. Given a string, if the first or last character is 'x', return the string without those 'x' characters,
-#else return the string unchanged. 
-#Example: If the input is "xHix", then output is "Hi".
+#4. Given a string, if the first or last character is 'x', return the string without those 'x' characters,else return the string unchanged.  Example: If the input is "xHix", then output is "Hi".
 
 s = "xHix"
 if s.startswith('x'):
@@ -32,8 +28,7 @@ if s.endswith('x'):
     s = s[:-1]
 print("Result:", s)
 
-# 5. Given a string and an integer n, return a string made of n repetitions of the last n characters of the string.
-# Example: If the inputs are “Wipro” and 3, then the output should be “propropro”.
+# 5. Given a string and an integer n, return a string made of n repetitions of the last n characters of the string.Example: If the inputs are “Wipro” and 3, then the output should be “propropro”.
 
 s = "Wipro"
 n = 3
